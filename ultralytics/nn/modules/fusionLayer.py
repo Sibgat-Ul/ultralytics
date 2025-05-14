571cf07f4d6d04ece7cbdf73e2d60262872c40c1
Sibgat-Ul_Islam <sibgatulislam@gmail.com>
2025-05-14 11:47:05 +0600

    improve performance

--- ultralytics/nn/modules/fusionLayer.py
+++ ultralytics/nn/modules/fusionLayer.py
@@ -18,26 +18,39 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
 class EarlyFusion(nn.Module):
     def __init__(self, c_inr=3, c_ini=1, c2=64, k=1, s=1, p=None, g=1, d=1, act=True):
         assert c2 % 2 is 0, "Output filters must be even"
-
         super().__init__()
-        self.rgb_conv1 = nn.Conv2d(c_inr, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
-        self.ir_conv1 = nn.Conv2d(c_ini, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
 
         half_filter = int(c2 / 2)
+        down_filter = int(half_filter/2)
+        self.rgb_conv1 = nn.Conv2d(c_inr, half_filter, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.ir_conv1 = nn.Conv2d(c_ini, half_filter, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+
 
         self.stem_block = nn.Sequential(
-            nn.Conv2d(c2, half_filter, kernel_size=1, stride=1),
-            nn.Conv2d(half_filter, c2, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(c2, eps=0.001, momentum=0.03, affine=True),
-            nn.SiLU(inplace=True)
+            nn.Conv2d(half_filter, down_filter, kernel_size=1, stride=1),
+            nn.Conv2d(down_filter, half_filter, kernel_size=3, stride=2, padding=1),
         )
+
+        self.bn = nn.BatchNorm2d(half_filter, eps=0.001, momentum=0.03, affine=True)
+        self.act = nn.SiLU(inplace=True)
 
     def forward(self, x):
         rgb_features = self.rgb_conv1(x[:, :3, :, :])
         ir_features = self.ir_conv1(x[:, 3:, :, :])
 
-        stem_output_rgb = self.stem_block(rgb_features)
-        stem_output_ir = self.stem_block(ir_features)
+        stem_output_rgb = self.act(self.bn(self.stem_block(rgb_features)))
+        stem_output_ir = self.act(self.bn(self.stem_block(ir_features)))
+
+        fused_features = torch.cat((stem_output_rgb, stem_output_ir), dim=1)
+
+        return fused_features
+
+    def forward_fuse(self, x):
+        rgb_features = self.rgb_conv1(x[:, :3, :, :])
+        ir_features = self.ir_conv1(x[:, 3:, :, :])
+
+        stem_output_rgb = self.act(self.stem_block(rgb_features))
+        stem_output_ir = self.act(self.stem_block(ir_features))
 
         fused_features = torch.cat((stem_output_rgb, stem_output_ir), dim=1)
 
