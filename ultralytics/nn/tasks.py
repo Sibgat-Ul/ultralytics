867aacd7cbcc653d1c20c49827dfdce15035d66c
Sibgat-Ul_Islam <sibgatulislam@gmail.com>
2025-05-14 10:11:04 +0600

    add modules to the list

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -68,7 +68,9 @@ from ultralytics.nn.modules import (
     YOLOEDetect,
     YOLOESegment,
     v10Detect,
+    EarlyFusion
 )
+
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
 from ultralytics.utils.loss import (
